c4ce5f76f5dece36421032de8890f9be1ef75af0
rodney-e-harris <90345548+rodney-e-harris@users.noreply.github.com>
2023-02-27 07:32:08 -0600

    Basic foundation for the homepage
    Moved navbar material and footer information to App.vue, created a placeholder logo, and made basic sections

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ul class="menu">
+    <div
+      class="menu-indicator"
+      :style="{ left: positionToMove, width: sliderWidth }"
+    ></div>
+    <li
+      class="menu-item"
+      v-for="link in links"
+      :key="link.id"
+      @click="sliderIndicator(link.id)"
+      :ref="'menu-item_' + link.id"
+    >
+      <a
+        href="#"
+        class="menu-link"
+        :class="link.id === selectedIndex ? 'active' : null"
+      >
+        <i class="menu-icon" :class="link.icon"></i>
+        <span>{{ link.text }}</span>
+      </a>
+    </li>
+  </ul> 
+
+  <HelloWorld msg="Data Analytics and Deep Learning Lab"/>
+  
 </template>
 
 <script>
@@ -10,7 +33,32 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+      sliderPosition: 0,
+      selectedElementWidth: 0,
+      selectedIndex: 0,
+      links: [
+        {
+          id: 1,
+          text: "Home",
+        },
+        {
+          id: 2,
+          text: "Research",
+        },
+        {
+          id: 3,
+          text: "Publications",
+        },
+        {
+          id: 4,
+          text: "People",
+        },
+      ],
+    };
+  },
 }
 </script>
 
@@ -22,5 +70,54 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+/* ul */
+.menu {
+  padding: 0;
+  margin: 0;
+  position: relative;
+  background-color: var(--menu-background-color);
+  display: inline-flex;
+  border-radius: 2px;
+  list-style-type: none;
+  overflow: hidden;
+}
+/* li */
+.menu-item {
+  display: inline-flex;
+}
+/* a */
+.menu-link {
+  padding: 0.75rem 1rem;
+  display: inline-flex;
+  align-items: center;
+  color: var(--link-text-color);
+  text-decoration: none;
+}
+.menu-link:hover,
+.menu-link.active {
+  color: var(--active-color);
+  background-color: var(--active-background-color);
+}
+/* icon */
+.menu-icon {
+  height: 1.5rem;
+  width: 1.5rem;
+  justify-content: center;
+  align-items: center;
+  display: inline-flex;
+  margin-right: 0.2rem;
+}
+/* slider */
+.menu-indicator {
+  position: absolute;
+  height: 0.25rem;
+  background-color: var(--active-color);
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  width: 3rem;
+  transition: all ease 0.5s;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,101 +1,40 @@
 <template>
     <div class="hello">
-
-      <ul class="menu">
-        <div
-          class="menu-indicator"
-          :style="{ left: positionToMove, width: sliderWidth }"
-        ></div>
-        <li
-          class="menu-item"
-          v-for="link in links"
-          :key="link.id"
-          @click="sliderIndicator(link.id)"
-          :ref="'menu-item_' + link.id"
-        >
-          <a
-            href="#"
-            class="menu-link"
-            :class="link.id === selectedIndex ? 'active' : null"
-          >
-            <i class="menu-icon" :class="link.icon"></i>
-            <span>{{ link.text }}</span>
-          </a>
-        </li>
-      </ul>
     
 
     <h1>{{ msg }}</h1>
+
+    <img alt="Vue logo" src="../assets/logo.png">
+
+
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      This would be the section for an explanation of the lab and the broader application areas.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h2>Application Areas</h2>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      Here we will go more indepth into each application area, perhaps providing image along with a small paragraph of text.
     </ul>
-    <h3>Essential Links</h3>
+    <h2>Recent News</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      Here we would have links related to news articles and blog posts about progress directly in the lab or the Deep Learning field.
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    
+    <h3>Footer Placeholders</h3>
+
+    <footer>
+      <ul>
+        <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">Github</a></li>
+        <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">Twitter</a></li>
+        <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">Come Visit Section</a></li>
+        <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">Contact Information</a></li>
+      </ul>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      sliderPosition: 0,
-      selectedElementWidth: 0,
-      selectedIndex: 0,
-      links: [
-        {
-          id: 1,
-          icon: "fab fa-react",
-          text: "React",
-        },
-        {
-          id: 2,
-          icon: "fab fa-angular",
-          text: "Angular",
-        },
-        {
-          id: 3,
-          icon: "fab fa-vuejs",
-          text: "Vue",
-        },
-        {
-          id: 4,
-          icon: "fab fa-html5",
-          text: "HTML",
-        },
-        {
-          id: 5,
-          icon: "fab fa-css3-alt",
-          text: "CSS3",
-        },
-        {
-          id: 6,
-          icon: "fab fa-js",
-          text: "Javascript",
-        },
-      ],
-    };
-  },
+  
   methods: {
     sliderIndicator(id) {
       let el = this.$refs[`menu-item_${id}`][0];
@@ -127,55 +66,6 @@ export default {
   --menu-background-color: #1d3557;
   --active-background-color: #132238;
 }
-/* ul */
-.menu {
-  padding: 0;
-  margin: 0;
-  position: relative;
-  background-color: var(--menu-background-color);
-  display: inline-flex;
-  border-radius: 4px;
-  list-style-type: none;
-  overflow: hidden;
-}
-/* li */
-.menu-item {
-  display: inline-flex;
-}
-/* a */
-.menu-link {
-  padding: 0.75rem 1rem;
-  display: inline-flex;
-  align-items: center;
-  color: var(--link-text-color);
-  text-decoration: none;
-}
-.menu-link:hover,
-.menu-link.active {
-  color: var(--active-color);
-  background-color: var(--active-background-color);
-}
-/* icon */
-.menu-icon {
-  height: 1.5rem;
-  width: 1.5rem;
-  justify-content: center;
-  align-items: center;
-  display: inline-flex;
-  margin-right: 0.2rem;
-}
-/* slider */
-.menu-indicator {
-  position: absolute;
-  height: 0.25rem;
-  background-color: var(--active-color);
-  bottom: 0;
-  left: 0;
-  margin: auto;
-  width: 3rem;
-  transition: all ease 0.5s;
-}
-
 
 h3 {
   margin: 40px 0 0;
