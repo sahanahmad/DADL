856b5ea7b193940c06a889959751256677ccbd89
rodney-e-harris <90345548+rodney-e-harris@users.noreply.github.com>
2023-03-17 09:30:42 -0500

    New React Code

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <h1>Data Analytics and Deep Learning Lab</h1>
+
+      {/* Displays the DADL Logo on the homepage  */}
+      <img src={require("./logo.png")}>
+      </img>
+    
+      {/* -- Area for broader explanations of project  */}
+      <p>
+        This would be the section for an explanation of the lab and the broader application areas.
+      </p>
+
+      {/* -- The Application areas will provide insight into the uses of the technology.
+      There will likely be image and paragraph sections for each application area. */}
+      <h2>Application Areas</h2>
+      <p>
+        Here we will go more indepth into each application area, perhaps providing image along with a small paragraph of text.
+      </p>
+
+      {/*-- The Recent News section should contain hyperlinks to relevant articles/blogs. There maybe text accompanying each article title, but
+      each link should ideally be sorted by the date of the article's creation  */}
+      <h2>Recent News</h2>
+      <p>
+        Here we would have links related to news articles and blog posts about progress directly in the lab or the Deep Learning field.
+      </p>
+
+
+      {/* -- The footer will contain sections where a user can find links to Github, relevant social medias and contact information */}
+      <h3>Footer Placeholders</h3>
+
+      <footer>
+        <ul>
+          <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">Github</a></li>
+          <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">Twitter</a></li>
+          <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">Come Visit Section</a></li>
+          <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">Contact Information</a></li>
+        </ul>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
